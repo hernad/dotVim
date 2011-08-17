ff4d15c39f18806b2965d6551c40d69d593b9b16
Ernad Husremovic <hernad@bring.out.ba>
2011-08-17 22:11:34 +0200

    easytags

--- submodule_add.sh
+++ submodule_add.sh
@@ -16,3 +16,5 @@ git submodule add https://github.com/majutsushi/tagbar.git bundle/tagbar
 git submodule add https://github.com/gmarik/vundle.git bundle/vundle
 git submodule add https://github.com/godlygeek/tabular.git bundle/tabular
 git submodule add https://github.com/scrooloose/nerdcommenter.git bundle/nerdcommenter
+git submodule add https://github.com/xolox/vim-easytags.git bundle/easytags
+
